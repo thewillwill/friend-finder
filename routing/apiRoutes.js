2232f726a41d0f57d4a009a1a41a6886a0c65f3a
William Brooks <brooks.william@gmail.com>
2017-11-17 07:50:33 -0800

    removed console logs from testing

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -44,7 +44,6 @@ module.exports = function(app) {
 
             }
 
-            console.log('scoreDiff', scoreDiff)
             //check if closer match
             if (scoreDiff < leastDiff) {
                 //record the new closest score
